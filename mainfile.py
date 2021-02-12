cc10947cbcaf9e63f2a3fc9b82354e8c7713f067
hari4742 <73326865+hari4742@users.noreply.github.com>
2021-02-13 00:43:42 +0530

    Update mainfile.py
    almost done

--- mainfile.py
+++ mainfile.py
@@ -1,24 +1,32 @@
-x = 15
-y = 2
-quotient = x//y
 power = 0
 bits = {}
+x = 9
+y = 2
+quotient = x//y
 while True:
     if x == 0:
        break
     elif quotient > 1:
-       y *= y
+       y *= 2
        power += 1
+       quotient = x//y
     elif quotient == 1:
        x = x % y
        power += 1
        bits[power]=1
        power = 0
+       y = 2
+       quotient = x//y
     else:
        bits[power] = 1
        break
- list = []
- for i in range(9):
+list = []
+for i in range(9):
     list.append (bits.get(i,0))
-
+list.reverse()
+firstoccur = list.index(1)
 print(list)
+print("finished")
+print(list.count(1))
+
+
