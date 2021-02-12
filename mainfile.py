8819a56943c87ec51dc4e415a52009b34053f289
hari4742 <73326865+hari4742@users.noreply.github.com>
2021-02-12 23:38:57 +0530

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -1,24 +1,24 @@
-def countbits(x):
-    y = 2
-    quotient = x//y
-    power = 0
-    bits = {}
-    while True:
-        if x == 0:
-            break
-        elif quotient > 1:
-            y *= y
-            power += 1
-        elif quotient == 1:
-            x = x % y
-            power += 1
-            bits[power]=1
-            power = 0
-        else:
-            bits[power] = 1
-            break
-    list = []
-    for i in range(9):
-        list.append (power.get(i,0))
-    return list
-print(countbits(15))
+x = 15
+y = 2
+quotient = x//y
+power = 0
+bits = {}
+while True:
+    if x == 0:
+       break
+    elif quotient > 1:
+       y *= y
+       power += 1
+    elif quotient == 1:
+       x = x % y
+       power += 1
+       bits[power]=1
+       power = 0
+    else:
+       bits[power] = 1
+       break
+ list = []
+ for i in range(9):
+    list.append (power.get(i,0))
+
+print(list)
